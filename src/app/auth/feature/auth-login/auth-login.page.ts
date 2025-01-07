9873fe4d4611c800f890eb2b4d15425a39f3ea24
PseudoCot <youkos00@mail.ru>
2025-01-07 21:17:46 +0300

    Update login and register forms

--- src/app/auth/feature/auth-login/auth-login.page.ts
+++ src/app/auth/feature/auth-login/auth-login.page.ts
@@ -1,42 +1,44 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { AuthService } from '../../data-access/auth.service';
 import { Router } from '@angular/router';
+import { ButtonComponent } from "../../../shared/ui/button/button.component";
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './auth-login.page.html',
-  styleUrl: './auth-login.page.scss',
+  styleUrls: ['./auth-login.page.scss', '../../../shared/ui/card/card.component.scss', '../../../shared/ui/form-input/form-input.component.scss'],
   imports: [
     CommonModule,
     ReactiveFormsModule,
+    ButtonComponent,
   ],
   standalone: true,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AuthLoginPage {
-  protected form: FormGroup;
+  protected _form: FormGroup;
 
-  constructor(private fb: FormBuilder,
-    private authService: AuthService,
-    private router: Router) {
-
-    this.form = this.fb.group({
-      email: ['', Validators.required],
+  constructor(
+    private fb: FormBuilder,
+    // private authService: AuthService,
+    private router: Router
+  ) {
+    this._form = this.fb.group({
+      login: ['', Validators.required],
       password: ['', Validators.required]
     });
   }
 
-  protected login() {
-    const val = this.form.value;
+  protected _login() {
+    const val = this._form.value;
 
     if (val.email && val.password) {
-      this.authService.login(val.email, val.password)
-        .subscribe(() => {
-          console.log("User is logged in");
-          this.router.navigateByUrl('/');
-        });
+      // this.authService.login(val.email, val.password)
+      //   .subscribe(() => {
+      //     console.log("User is logged in");
+      //     this.router.navigateByUrl('/');
+      //   });
     }
   }
 }
--- src/app/auth/feature/auth-register/auth-register.page.ts
+++ src/app/auth/feature/auth-register/auth-register.page.ts
@@ -1,14 +1,47 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ButtonComponent } from "../../../shared/ui/button/button.component";
 
 @Component({
   selector: 'app-register-page',
   templateUrl: './auth-register.page.html',
-  styleUrl: './auth-register.page.scss',
+  styleUrls: ['./auth-register.page.scss', '../../../shared/ui/card/card.component.scss'],
   imports: [
     CommonModule,
+    ReactiveFormsModule,
+    ButtonComponent,
   ],
   standalone: true,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AuthRegisterPage { }
+export class AuthRegisterPage {
+  protected _form: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    // private authService: AuthService,
+    private router: Router
+  ) {
+    this._form = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', Validators.required, Validators.email],
+      nickname: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  protected _register() {
+    const val = this._form.value;
+
+    if (val.email && val.password) {
+      // this.authService.login(val.email, val.password)
+      //   .subscribe(() => {
+      //     console.log("User is logged in");
+      //     this.router.navigateByUrl('/');
+      //   });
+    }
+  }
+}
+
