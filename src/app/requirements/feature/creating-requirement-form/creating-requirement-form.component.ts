f47a7aeb6eadd8607ee40269a4f23ca61d097606
PseudoCot <youkos00@mail.ru>
2025-01-10 02:09:32 +0300

    Update requirement creating form

--- src/app/requirements/feature/creating-requirement-form/creating-requirement-form.component.ts
+++ src/app/requirements/feature/creating-requirement-form/creating-requirement-form.component.ts
@@ -1,13 +1,25 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { ModalWindowComponent } from "../../../shared/ui/modal-window/modal-window.component";
 import { SvgIconComponent } from "../../../shared/ui/svg-icon/svg-icon.component";
+import { ButtonComponent } from "../../../shared/ui/button/button.component";
+import { UserDataMock } from '../../../shared/models/mock-data/user.data-mock';
 
 @Component({
   selector: 'app-creating-requirement-form',
   templateUrl: './creating-requirement-form.component.html',
-  styleUrls: ['./creating-requirement-form.component.scss'],
+  styleUrls: [
+    './creating-requirement-form.component.scss',
+    '../../../shared/ui/modal-card/modal-card.component.scss',
+    '../../../shared/ui/requirement-entity/requirement-entity.component.scss',
+    '../../../shared/ui/select/select.component.scss',
+  ],
   changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: true,
-  imports: [ModalWindowComponent, SvgIconComponent],
+  imports: [ModalWindowComponent, SvgIconComponent, ButtonComponent],
 })
-export class CreatingRequirementFormComponent { }
+export class CreatingRequirementFormComponent {
+  protected usersData = UserDataMock;
+
+  @Input({ required: true }) show$!: BehaviorSubject<boolean>;
+}
